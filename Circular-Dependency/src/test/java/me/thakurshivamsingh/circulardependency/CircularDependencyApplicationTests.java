22d5c63757035dd985afd2d7fdfed2992e458014
SHIVAM <dreamyshivam01@gmail.com>
2026-06-25 22:58:34 +0530

    Circular Dependency Problem Demo .

---
+++ Circular-Dependency/src/test/java/me/thakurshivamsingh/circulardependency/CircularDependencyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.thakurshivamsingh.circulardependency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircularDependencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
