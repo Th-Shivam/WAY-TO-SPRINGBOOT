56e82787836372e6ef63b0d60d200d83a8a1ea7b
SHIVAM <dreamyshivam01@gmail.com>
2026-07-19 23:12:11 +0530

    POJO Student class

--- SimpleCRUD/src/main/java/me/thakurshivamsingh/simplecrud/entity/Student.java
+++ SimpleCRUD/src/main/java/me/thakurshivamsingh/simplecrud/entity/Student.java
@@ -1,4 +1,50 @@
 package me.thakurshivamsingh.simplecrud.entity;
 
 public class Student {
+
+    private String Name;
+    private String email;
+    private int roll ;
+    private int age;
+    private String Subject ;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getSubject() {
+        return Subject;
+    }
+
+    public void setSubject(String subject) {
+        Subject = subject;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
