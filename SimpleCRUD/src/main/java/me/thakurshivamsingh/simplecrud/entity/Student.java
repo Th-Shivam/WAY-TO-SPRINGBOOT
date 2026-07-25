27edbf489024e4bcc4df2e9e4e806a2929984664
SHIVAM <dreamyshivam01@gmail.com>
2026-07-25 23:43:53 +0530

    Auto increment the primary key i.e. id

--- SimpleCRUD/src/main/java/me/thakurshivamsingh/simplecrud/entity/Student.java
+++ SimpleCRUD/src/main/java/me/thakurshivamsingh/simplecrud/entity/Student.java
@@ -1,11 +1,14 @@
 package me.thakurshivamsingh.simplecrud.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Student {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private int age;
